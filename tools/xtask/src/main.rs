550da5240f0f222b82780835921aaf7220b9ec01
Daniel Bittman <danielbittman1@gmail.com>
2021-12-17 13:10:53 -0800

    support profiles

--- tools/xtask/src/main.rs
+++ tools/xtask/src/main.rs
@@ -1,22 +1,35 @@
-use std::{env, process::Command};
+use std::{env, process::Command, str::FromStr};
 
-use cargo_metadata::MetadataCommand;
+use cargo_metadata::{Metadata, MetadataCommand};
 
 type DynError = Box<dyn std::error::Error>;
 
 fn main() {
-    if let Err(_e) = try_main() {
-        // eprintln!("{}", e);
+    if let Err(e) = try_main() {
+        eprintln!("{}", e);
         std::process::exit(101);
     }
 }
 
+#[derive(Copy, Clone, Debug)]
+enum Profile {
+    Debug,
+    Release,
+}
 fn try_main() -> Result<(), DynError> {
+    let path = "Cargo.toml";
+    let meta = MetadataCommand::new().manifest_path(path).exec().unwrap();
     let task = env::args().nth(1);
+    let args = &env::args().into_iter().collect::<Vec<String>>()[2..];
+    let profile = if args.contains(&"--release".to_owned()) {
+        Profile::Release
+    } else {
+        Profile::Debug
+    };
     match task.as_ref().map(|it| it.as_str()) {
-        Some("build-all") => build_all()?,
-        Some("check-all") => check_all()?,
-        Some("make-disk") => make_disk()?,
+        Some("build-all") => build_all(&meta, args, profile)?,
+        Some("check-all") => check_all(&meta, args, profile)?,
+        Some("make-disk") => make_disk(&meta, args, profile)?,
         _ => print_help(),
     }
     Ok(())
@@ -26,59 +39,78 @@ fn print_help() {
     println!("xtask help TODO")
 }
 
-fn check_all() -> Result<(), DynError> {
+fn cargo_cmd_collection(
+    meta: &Metadata,
+    collection_name: &str,
+    cargo_cmd: &str,
+    wd: &str,
+    args: &[String],
+    profile: Profile,
+) -> Result<(), DynError> {
+    eprintln!(
+        "== BUILDING COLLECTION {} ({:?}) ==",
+        collection_name, profile
+    );
     let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
-    let status1 = Command::new(cargo)
-        .current_dir("src/kernel")
-        .args(&["check", "--message-format=json"])
-        .status()?;
-    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
-    let status2 = Command::new(cargo)
-        .args(&[
-            "check",
-            "--message-format=json",
-            "--workspace",
-            "--exclude",
-            "rustkernel",
-        ])
+    let pkg_list: Vec<String> = meta.workspace_metadata[collection_name]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|x| {
+            [
+                String::from_str("--bin").unwrap(),
+                x.to_string().replace("\"", ""),
+            ]
+        })
+        .flatten()
+        .collect();
+    //println!("{:?}", pkg_list);
+    let status = Command::new(cargo)
+        .current_dir(wd)
+        .arg(cargo_cmd)
+        .args(pkg_list)
+        .args(args)
         .status()?;
 
-    if !status1.success() {
-        Err("cargo check failed")?;
-    }
-    if !status2.success() {
-        Err("cargo check failed")?;
+    if !status.success() {
+        Err("failed to run cargo command")?;
     }
     Ok(())
 }
 
-fn build_all() -> Result<(), DynError> {
-    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
-    let status = Command::new(cargo)
-        .current_dir("src/kernel")
-        .args(&["build"])
-        .status()?;
-
-    if !status.success() {
-        Err("cargo build failed")?;
-    }
-    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
-    let status = Command::new(cargo)
-        .args(&["build", "--workspace", "--exclude", "rustkernel"])
-        .status()?;
-
-    if !status.success() {
-        Err("cargo build failed")?;
-    }
+fn cmd_all(
+    meta: &Metadata,
+    args: &[String],
+    cargo_cmd: &str,
+    profile: Profile,
+) -> Result<(), DynError> {
+    cargo_cmd_collection(meta, "tools", cargo_cmd, ".", args, profile)?;
+    cargo_cmd_collection(meta, "kernel", cargo_cmd, "src/kernel", args, profile)?;
+    cargo_cmd_collection(meta, "initrd-members", cargo_cmd, ".", args, profile)?;
     Ok(())
 }
 
-fn make_disk() -> Result<(), DynError> {
-    let path = "Cargo.toml";
-    let _meta = MetadataCommand::new().manifest_path(path).exec().unwrap();
-    build_all()?;
-    let status = Command::new("target/debug/image_builder")
-        .arg("target/x86_64-pc-none/debug/rustkernel")
+fn check_all(meta: &Metadata, args: &[String], profile: Profile) -> Result<(), DynError> {
+    cmd_all(meta, args, "check", profile)?;
+    Ok(())
+}
+
+fn build_all(meta: &Metadata, args: &[String], profile: Profile) -> Result<(), DynError> {
+    cmd_all(meta, args, "build", profile)?;
+    Ok(())
+}
+
+fn make_disk(meta: &Metadata, args: &[String], profile: Profile) -> Result<(), DynError> {
+    build_all(meta, args, profile)?;
+    let profile_path = match profile {
+        Profile::Debug => "debug",
+        Profile::Release => "release",
+    };
+    let status = Command::new(format!("target/{}/image_builder", profile_path))
+        .arg(format!(
+            "target/x86_64-pc-none/{}/twizzler-kernel",
+            profile_path
+        ))
         .status()?;
 
     if !status.success() {
