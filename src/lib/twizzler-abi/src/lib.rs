9e789511e1b28cd0174b27177e8262a53df1dada
Daniel Bittman <danielbittman1@gmail.com>
2022-01-28 20:12:53 +0000

    Fix docs error, pt 2.

--- src/lib/twizzler-abi/src/lib.rs
+++ src/lib/twizzler-abi/src/lib.rs
@@ -1,7 +1,14 @@
 //! This library provides a common interface for applications that want to talk to the Twizzler
 //! kernel, and defines that interface for both applications and the kernel to follow. It's made of
 //! several parts:
-//!   1. The Runtime -- see [rt1]
+//!   1. The Runtime -- see [rt1], [aux], and [exec]
+//!   2. System Calls -- see [syscall] and [arch::syscall]
+//!   3. The rest, acting as support for the rust standard library.
+//!
+//! **Note**: All of these interfaces are potentially unstable and should not be used directly by most
+//! programs. Instead, the twizzler crate can be used to access standard Twizzler object APIs, and
+//! the rust standard library should be used for general rust programming. Both of these wrap calls
+//! to this library in better, more easily consumed APIs.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(asm)]
@@ -23,8 +30,11 @@ pub mod slot;
 pub mod syscall;
 pub mod time;
 
+/// Simple callback into twizzler_abi made by the standard library once it has initialized the
+/// environment. No panic runtime is available yet.
 pub fn ready() {}
 
+/// We need to provide a no-mangled abort() call for things like libunwind.
 #[no_mangle]
 pub extern "C" fn abort() -> ! {
     unsafe { internal_abort() }
@@ -39,12 +49,17 @@ fn print_err(err: &str) {
     syscall::sys_kernel_console_write(err.as_bytes(), syscall::KernelConsoleWriteFlags::empty());
 }
 
+/// We need to provide a basic hook for reporting stack check failure in case we link to a library
+/// that assumes stack protections works (libunwind).
 #[no_mangle]
 pub unsafe extern "C" fn __stack_chk_fail() {
     print_err("stack overflow -- aborting");
     abort();
 }
 
+/// During runtime init, we need to call functions that might fail, but if they do so, we should
+/// just abort. The standard unwrap() function for Option will call panic, but we can't use that, as
+/// the runtime init stuff runs before the panic runtime is ready.
 fn internal_unwrap<T>(t: Option<T>, msg: &str) -> T {
     if let Some(t) = t {
         t
