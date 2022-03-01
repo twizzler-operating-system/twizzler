715afbe98c22ce52f5efee878ea9950c28ad2913
Daniel Bittman <danielbittman1@gmail.com>
2022-03-01 04:31:27 +0000

    Implement basic flag blocking.

--- src/lib/twizzler-async/src/future.rs
+++ src/lib/twizzler-async/src/future.rs
@@ -1,4 +1,8 @@
-use std::{future::Future, task::Poll};
+use std::{
+    future::Future,
+    sync::{Arc, Mutex},
+    task::{Poll, Waker},
+};
 
 use futures_util::FutureExt;
 
@@ -47,5 +51,69 @@ impl<FutOne: Future + Unpin, FutTwo: Future + Unpin> Future for WaitForFirst<Fut
         }
 
         Poll::Pending
+    }
+}
+
+pub struct FlagBlockInner {
+    wakers: Vec<Waker>,
+    epoch: u64,
+}
+
+pub struct FlagBlock {
+    inner: Arc<Mutex<FlagBlockInner>>,
+}
+
+pub struct FlagBlockFuture<'a> {
+    state: &'a FlagBlock,
+    val: u64,
+    added: bool,
+}
+
+impl FlagBlock {
+    pub fn new() -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(FlagBlockInner {
+                wakers: vec![],
+                epoch: 0,
+            })),
+        }
+    }
+
+    pub fn signal_all(&self) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.epoch += 1;
+        while let Some(w) = inner.wakers.pop() {
+            w.wake();
+        }
+    }
+
+    pub fn wait(&self) -> FlagBlockFuture {
+        let inner = self.inner.lock().unwrap();
+        FlagBlockFuture {
+            state: self,
+            added: false,
+            val: inner.epoch,
+        }
+    }
+}
+
+impl<'a> Future for FlagBlockFuture<'a> {
+    type Output = ();
+
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Self::Output> {
+        let mut inner = self.state.inner.lock().unwrap();
+        if inner.epoch != self.val {
+            Poll::Ready(())
+        } else {
+            if !self.added {
+                inner.wakers.push(cx.waker().clone());
+                drop(inner);
+                self.added = true;
+            }
+            Poll::Pending
+        }
     }
 }
