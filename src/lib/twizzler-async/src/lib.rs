125bf473fc601264f88423d0f3ce2886f9d62a51
Daniel Bittman <danielbittman1@gmail.com>
2022-03-04 05:31:01 +0000

    Working on docs.

--- src/lib/twizzler-async/src/lib.rs
+++ src/lib/twizzler-async/src/lib.rs
@@ -1,3 +1,39 @@
+//! Support for asynchronous programming on Twizzler. This crate provides executor functionality
+//! along with support for async events and waiting, timers and timeouts, and a couple general
+//! helper functions.
+//!
+//! # Executors
+//! We provide three types of executors:
+//!   1. block_on, which blocks until the future is completed.
+//!   2. Thread-local, for futures that aren't Send.
+//!   3. Global, which puts tasks in a global scheduling context for thread pools to handle.
+//!
+//! # Examples
+//! The most basic way to run a future is:
+//! ```
+//! let result = block_on(async { /* some async code */ });
+//! ```
+//!
+//! But this of course doesn't really make it possible to actually run things concurrently, since it
+//! just waits for this single future. Instead, you probably want to use a real executor. The main
+//! one you probably want is the global executor:
+//! ```
+//! let result = Task::spawn(async { /* some async code */ }).await;
+//! ```
+//! Now, this does assume that there is a thread that has called [crate::run], eg:
+//! ```
+//! let result = run(async { Task::spawn(async { /* some async code */ }).await });
+//! ```
+//!
+//! Generally, though, if you want a thread pool, you can spawn a thread into a pool like this:
+//! ```
+//! std::thread::spawn(|| twizzler_async::run(std::future::pending::<()>()));
+//! ```
+//!
+//! Then, later on, you can spawn a Task and await it. You can also detach a Task with .detach(),
+//! which just places the thread on the runqueues and runs it without you having to await the result.
+//!
+
 mod async_source;
 mod block_on;
 mod event;
