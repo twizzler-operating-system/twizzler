56e5e53a48381a34859a376960adaa0485bc8827
Allen Aboytes <aaboytes@ucsc.edu>
2022-08-27 01:43:24 +0000

    Added documentation to clock list syscall

--- src/lib/twizzler-abi/src/syscall/time/mod.rs
+++ src/lib/twizzler-abi/src/syscall/time/mod.rs
@@ -108,14 +108,18 @@ impl std::error::Error for ReadClockListError {
 }
 
 bitflags! {
-    /// Flags to pass to [sys_read_clock_info].
+    /// Flags to pass to [`sys_read_clock_info`].
     pub struct ReadClockFlags: u32 {
 
     }
 
+    /// Flags to pass to [`sys_read_clock_list`].
     pub struct ReadClockListFlags: u32 {
+        /// Fill the buffer with all clocks from the clock list, for every `ClockKind`.
         const ALL_CLOCKS = 1 << 0;
+        /// Fill the buffer with only clocks from a given `ClockKind` list.
         const ONLY_KIND = 1 << 1;
+        /// Fill the buffer with the first clock in the `ClockKind` list.
         const FIRST_KIND = 1 << 2;
     }
 }
@@ -175,6 +179,31 @@ pub fn sys_read_clock_info(
 }
 
 /// Discover a list of clock sources exposed by the kernel.
+///
+/// This returns a list of clocks stored in `clocks` and the number of
+/// entries filled. By default, one clock from every type of clock
+/// exposed ([`ClockKind`]), is returned. All information in [`ClockInfo`]
+/// except the current value is also returned. For each type of clock with more
+/// than one clock source, the first one is returned. Users can get a list of
+/// all clocks, and thus all clock sources, for a particular type by
+/// specifying the [`ClockKind`] and setting the appropriate flag.
+///
+/// Users are expected to provide a slice, `clocks`, to be filled by the kernel.
+/// `start` indicates what offset into the list of clocks the kernel should fill
+/// the `clocks` buffer from. When there are no more clocks to read from a given
+/// `start` offset, then the value 0 is returned.
+///
+/// # Examples
+///
+/// ```no_run
+/// let mut clocks = [Clock::ZERO; 4];
+/// let result = sys_read_clock_list(ClockKind::Monotonic, &mut clocks, 0, ReadClockListFlags::FIRST_KIND);
+/// if let Some(filled) = result {
+///     if filled > 0 {
+///         println!("time now: {}", clock[0].read().as_nanos());
+///     }
+/// }
+/// ```
 pub fn sys_read_clock_list(
     clock: ClockKind,
     clocks: &mut [Clock],
