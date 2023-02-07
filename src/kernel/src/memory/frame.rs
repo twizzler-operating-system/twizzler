72305ba1f6cce652f9b4bbf6b12922c2c65fba2b
Daniel Bittman <danielbittman1@gmail.com>
2023-02-07 12:23:06 -0800

    Add comment on intrusive data structures.

--- src/kernel/src/memory/frame.rs
+++ src/kernel/src/memory/frame.rs
@@ -24,6 +24,13 @@
 //! needs to be able to access frame data from possibly any CPU, so the whole type must be both Sync
 //! and Send. This would be easy with the lock-around-inner trick, but this plays badly with the
 //! intrusive list, and so we do some cursed manual locking to ensure write isolation.
+//!
+//! Note: This code uses intrusive linked lists (a type of intrusive data structure). These are standard
+//! practice in C kernels, but are rarely needed these days. An intrusive list is a list that stores the
+//! list's link data inside the nodes (`struct Foo {link: Link, ...}`) as opposed to storing the objects in
+//! the list (`struct ListItem<T> {item: T, link: Link}`). They are useful here because they can form
+//! arbitrary containers while ensuring no memory is allocated to store the list, something that is very
+//! important inside an allocator for physical pages. For more information, see: [https://docs.rs/intrusive-collections/latest/intrusive_collections/].
 
 use core::{
     intrinsics::size_of,
