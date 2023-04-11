5ddeb1847d7cea76a0161cb76967359a8b22658a
Allen Aboytes <aaboytes@ucsc.edu>
2023-04-11 11:32:37 -0700

    Implement physical frame allocation for aarch64 (#119)
    * Pass physical memory regions from the bootloader to frame allocator
    * Implement architecture specific info needed for `PhysAddr/VirtAddr`
    * Clean up kernel entry point to call `kernel_main`
    * Enable/disable IRQ so we can used `logln!`
    * Add descriptive names for DAIF mask bits

--- src/kernel/src/arch/aarch64/memory.rs
+++ src/kernel/src/arch/aarch64/memory.rs
@@ -4,12 +4,16 @@ use super::address::{PhysAddr, VirtAddr};
 pub mod frame;
 pub mod pagetables;
 
-// TODO:
-// start offset into physical memory
-const PHYS_MEM_OFFSET: u64 = 0x0;
+// start offset into physical memory. 
+// 
+// in the future we should determine this at runtime 
+// since we don't know what the CPU supports. We might
+// go about this by making `PhysAddr::get_phys_addr_width()`
+// public and then calculate it that way. For now we assume
+// a 48-bit physical address space.
+const PHYS_MEM_OFFSET: u64 = 0xffff800000000000;
 
 /* TODO: hide this */
 pub fn phys_to_virt(pa: PhysAddr) -> VirtAddr {
-    let raw: u64 = pa.into();
-    VirtAddr::new(raw + PHYS_MEM_OFFSET).unwrap()
+    VirtAddr::new(pa.raw() + PHYS_MEM_OFFSET).unwrap()
 }
--- src/kernel/src/arch/aarch64/memory/frame.rs
+++ src/kernel/src/arch/aarch64/memory/frame.rs
@@ -1,3 +1,5 @@
-// TODO:
-// arch specific frame (page) size
+// arch specific frame (page) size in bytes.
+// Frame size is chosen from translation granule.
+// In this implementation we go with 4 KiB pages.
+// In the future we could determine this at runtime.
 pub const FRAME_SIZE: usize = 0x1000;
