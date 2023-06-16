ae829ebf2caaee429f4177cb138c4d67969e3adf
Allen Aboytes <aaboytes@ucsc.edu>
2023-06-16 18:11:45 +0000

    Add check for console to not use UART before memory management is initialized

--- src/kernel/src/machine/arm/virt/serial.rs
+++ src/kernel/src/machine/arm/virt/serial.rs
@@ -13,9 +13,7 @@ lazy_static! {
     // TODO: add a spinlock here
     pub static ref SERIAL: PL011 = {
         // the desired virtal address for this region of mmio
-        // let uart_mmio_base = VirtAddr::new(0xFFFF_FFFF_FFFF_0000)
-        //     .expect("invalid virtual address");
-        let uart_mmio_base = PL011_UART.start.kernel_vaddr();
+        let uart_mmio_base = VirtAddr::new(0xFFFF_0000_0000_0000).unwrap();
         // configure mapping settings for this region of memory
         let cursor = MappingCursor::new(
             uart_mmio_base,
@@ -54,20 +52,28 @@ impl PL011 {
         }
     }
 
-    // initalize the uart driver early, before interrupts are enabled
+    /// initalize the UART driver early, before interrupts in the system are enabled
     fn early_init(&self) {
         const CLOCK: u32 = 0x16e3600; // 24 MHz, TODO: get clock rate
         const BAUD: u32 = 115200;
-        unsafe { self.init(CLOCK, BAUD); }
+        // configure the UART with the desired baud, given the clock rate
+        unsafe { 
+            self.init(CLOCK, BAUD); 
+        }
     }
 }
 
 pub fn write(data: &[u8], _flags: crate::log::KernelConsoleWriteFlags) {
     // We need the memory management system up and running to use MMIO.
+    // Other requests to log to the console are ignored. The console is
+    // initialized lazily on first access.
+    //
     // This means that we cannot and should not ouput logging messages to
-    // the UART before this happens. For something like QEMU, we can get
-    // away with not mapping MMIO properly, but not real hardware. 
-    unsafe {
-        SERIAL.write_str(core::str::from_utf8_unchecked(data));
+    // the UART before this happens. Mapping in some memory might require
+    // allocating physical frames for the page tables.
+    if crate::memory::is_init() {
+        unsafe {
+            SERIAL.write_str(core::str::from_utf8_unchecked(data));
+        }
     }
 }
