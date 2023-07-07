9fc25c9255d4ac2d39cd99e64e7ead5554c66e85
Allen Aboytes <aaboytes@ucsc.edu>
2023-07-06 17:52:38 -0700

    Initialize GPIO pins 14/15 for UART output

--- src/kernel/src/machine/arm/rpi4/mod.rs
+++ src/kernel/src/machine/arm/rpi4/mod.rs
@@ -1,6 +1,9 @@
+mod gpio;
 pub mod interrupt;
 pub mod memory;
 pub mod serial;
+
+pub mod tests;
 
 pub fn machine_post_init() {
     // TODO: initialize uart with interrupts
--- src/kernel/src/machine/arm/rpi4/serial.rs
+++ src/kernel/src/machine/arm/rpi4/serial.rs
@@ -1,7 +1,10 @@
 use lazy_static::lazy_static;
 use twizzler_abi::{device::CacheType, object::Protections};
 
-use super::super::uart::PL011;        
+use super::{
+    super::uart::PL011,
+    gpio::{GpioPin, FunctionOperation, PullSelect, PIN_MANAGER},
+};
 use super::memory::mmio::PL011_UART;
 
 use crate::memory::{VirtAddr, pagetables::{
@@ -12,7 +15,7 @@ use crate::memory::{VirtAddr, pagetables::{
 lazy_static! {
     // TODO: add a spinlock here
     pub static ref SERIAL: PL011 = {
-        // the desired virtal address for this region of mmio
+        // the desired virtal address for the UART's MMIO
         let uart_mmio_base = VirtAddr::new(0xFFFF_0000_0000_0000).unwrap();
         // configure mapping settings for this region of memory
         let cursor = MappingCursor::new(
@@ -23,7 +26,8 @@ lazy_static! {
             PL011_UART.start,
             PL011_UART.length,
         );
-        // Device memory only prevetns speculative data accesses, so we must not
+
+        // Device memory only prevents speculative data accesses, so we must not
         // make this region executable to prevent speculative instruction accesses.
         let settings = MappingSettings::new(
             Protections::READ | Protections::WRITE,
@@ -41,6 +45,15 @@ lazy_static! {
             PL011::new(uart_mmio_base.into()) 
         };
         serial_port.early_init();
+
+        // Now that the UART object is ready, we must configure how we will
+        // output the data from the device. We reserve GPIO pins 14/15 (TX/RX)
+        // for this purpose.
+        PIN_MANAGER.set_function(GpioPin::Pin14, FunctionOperation::AlternateFunction0);
+        PIN_MANAGER.set_function(GpioPin::Pin15, FunctionOperation::AlternateFunction0);
+        PIN_MANAGER.set_pull(GpioPin::Pin14, PullSelect::NoResistor);
+        PIN_MANAGER.set_pull(GpioPin::Pin15, PullSelect::NoResistor);
+
         serial_port
     };
 }
@@ -54,7 +67,9 @@ impl PL011 {
 
     /// initalize the UART driver early, before interrupts in the system are enabled
     fn early_init(&self) {
-        const CLOCK: u32 = 0x16e3600; // 24 MHz, TODO: get clock rate
+        // 24 MHz, TODO: get clock rate (set statically in config.txt)
+        // let's get the actual value from the firmware!!! 48000000
+        const CLOCK: u32 = 48000000; 
         const BAUD: u32 = 115200;
         // configure the UART with the desired baud, given the clock rate
         unsafe { 
