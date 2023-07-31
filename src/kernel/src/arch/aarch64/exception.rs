7fd3371850e94eb634ea0952d3dd538d95343ef5
Allen Aboytes <aaboytes@ucsc.edu>
2023-07-31 12:17:33 -0700

    Implement page fault handler in kernel

--- src/kernel/src/arch/aarch64/exception.rs
+++ src/kernel/src/arch/aarch64/exception.rs
@@ -49,7 +49,7 @@ b default_exception_handler
 // Taking an exception from the current EL with SP_EL1 (kernel)
 // The exception is handled from EL1->EL1. The stack pointer from
 // the kernel is preserved.
-b default_exception_handler
+b sync_exception_handler
 .align {VECTOR_ALIGNMENT}
 b interrupt_request_handler
 .align {VECTOR_ALIGNMENT}
@@ -301,6 +301,73 @@ fn debug_handler(ctx: &mut ExceptionContext) {
     emerglog!("[kernel::exception] dumping register state: {}", ctx);
 
     panic!("caught unhandled exception!!!")
+}
+
+// Exception handler that services synchronous exceptions
+exception_handler!(sync_exception_handler, sync_handler);
+
+/// Exception handler deals with synchronous exceptions
+/// such as Data Aborts (i.e. page faults)
+fn sync_handler(ctx: &mut ExceptionContext) {
+    // read of raw value for ESR
+    let esr = ESR_EL1.get();
+    let esr_reg: InMemoryRegister<u64, ESR_EL1::Register> = InMemoryRegister::new(esr);
+    match esr_reg.read_as_enum(ESR_EL1::EC) {
+        Some(ESR_EL1::EC::Value::DataAbortCurrentEL) => {
+            // iss: syndrome
+            let iss = esr_reg.read(ESR_EL1::ISS);
+            // is the fault address register valid?
+            let far_valid = iss & (1 << 10) == 0;
+            // print faulting address (ELR/FAR)
+            let far = arm64::registers::FAR_EL1.get();
+            if !far_valid {
+                panic!("FAR is not valid!!");
+            }
+
+            // was fault caused by a write to memory or a read?
+            let write_fault = iss & (1 << 6) != 0;
+            let cause = if write_fault {
+                MemoryAccessKind::Write
+            } else {
+                MemoryAccessKind::Read
+            };
+
+            // TODO: support for PRESENT and INVALID flags
+            let flags = PageFaultFlags::empty();
+
+            let far_va = VirtAddr::new(far as u64).unwrap();
+
+            // DFSC bits[5:0] indicate the type of fault
+            let dfsc = iss & 0b111111;
+            if dfsc & 0b111100 == 0b001000 {
+                // we have an access fault
+                let level = dfsc & 0b11;
+                todo!("Access flag fault, level {}", level);
+                // TODO: set the access flag
+            } else if dfsc & 0b001100 == 0b001100 {
+                let level = dfsc & 0b11;
+                todo!("Permission fault, level {}", level);
+            }
+            crate::thread::enter_kernel();
+            // crate::interrupt::set(true);
+            let elr = ELR_EL1.get();
+            if let Ok(elr_va) = VirtAddr::new(elr) {
+                crate::memory::context::virtmem::page_fault(
+                    far_va,
+                    cause,
+                    flags,
+                    elr_va,
+                );
+            } else {
+                todo!("send upcall exception info");
+            }
+            // crate::interrupt::set(false);
+            crate::thread::exit_kernel();
+        },
+        Some(ESR_EL1::EC::Value::Unknown) | _ => {
+            debug_handler(ctx)
+        },
+    }
 }
 
 /// Initializes the exception vector table by writing the address of 
