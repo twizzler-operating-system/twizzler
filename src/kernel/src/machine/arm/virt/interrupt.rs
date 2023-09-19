08cf221ec6dac43338fc08a3a5ad8c9b1c8307bc
Allen Aboytes <aaboytes@ucsc.edu>
2023-09-19 12:01:05 -0700

    Find GICv2 MMIO using device tree

--- src/kernel/src/machine/arm/virt/interrupt.rs
+++ src/kernel/src/machine/arm/virt/interrupt.rs
@@ -2,38 +2,38 @@ use lazy_static::lazy_static;
 
 use super::super::common::gicv2::GICv2;
 
-use crate::machine::memory::mmio::{GICV2_DISTRIBUTOR, GICV2_CPU_INTERFACE};
-
 lazy_static! {
     /// System-wide reference to the interrupt controller
     pub static ref INTERRUPT_CONTROLLER: GICv2 = {
         use twizzler_abi::{device::CacheType, object::Protections};
         
         use crate::memory::{
-            VirtAddr,
+            VirtAddr, PhysAddr,
             pagetables::{
                 ContiguousProvider, MappingCursor, MappingSettings, Mapper,
                 MappingFlags,
             },
         };
+        // TODO: allocate mmio addresses ... 
         let gicc_mmio_base = VirtAddr::new(0xFFFF_0000_0000_2000).unwrap();
-        let gicd_mmio_base = VirtAddr::new(0xFFFF_0000_0000_4000).unwrap();
+        let gicd_mmio_base = VirtAddr::new(0xFFFF_0000_0001_3000).unwrap();
+        let (distributor_mmio, cpu_interface_mmio) = crate::machine::info::get_gicv2_info();
         // configure mapping settings for this region of memory
         let gicc_region = MappingCursor::new(
             gicc_mmio_base,
-            GICV2_CPU_INTERFACE.length,
+            cpu_interface_mmio.length as usize,
         );
         let mut gicc_phys = ContiguousProvider::new(
-            GICV2_CPU_INTERFACE.start,
-            GICV2_CPU_INTERFACE.length,
+            unsafe { PhysAddr::new_unchecked(cpu_interface_mmio.info) },
+            cpu_interface_mmio.length as usize,
         );
         let gicd_region = MappingCursor::new(
             gicd_mmio_base,
-            GICV2_DISTRIBUTOR.length,
+            distributor_mmio.length as usize,
         );
         let mut gicd_phys = ContiguousProvider::new(
-            GICV2_DISTRIBUTOR.start,
-            GICV2_DISTRIBUTOR.length,
+            unsafe { PhysAddr::new_unchecked(distributor_mmio.info) },
+            distributor_mmio.length as usize,
         );
         // Device memory only prevetns speculative data accesses, so we must not
         // make this region executable to prevent speculative instruction accesses.
--- src/kernel/src/machine/arm/virt/memory.rs
+++ src/kernel/src/machine/arm/virt/memory.rs
@@ -1,35 +1,3 @@
-// This interface is temporary until we can utilize something
-// like a Device Tree or ACPI during boot to describe the memory
-// map in addition to the memory map the bootloader gives us.
-pub mod mmio {
-    use crate::memory::{MemoryRegion, MemoryRegionKind, PhysAddr};
-
-    /// The region of the physical memory map that represents
-    /// the MMIO registers of the GICv2 Distributor
-    pub const GICV2_DISTRIBUTOR: MemoryRegion = MemoryRegion {
-        // physical base address in QEMU
-        start: unsafe {
-            PhysAddr::new_unchecked(0x08000000)
-        },
-        // Distributor interface register map goes from 0x0000-0x1000
-        length: 0x00001000,
-        kind: MemoryRegionKind::Reserved,
-    };
-
-    /// The region of the physical memory map that represents
-    /// the MMIO registers of the GICv2 CPU Interface
-    pub const GICV2_CPU_INTERFACE: MemoryRegion = MemoryRegion {
-        // physical base address in QEMU
-        start: unsafe {
-            PhysAddr::new_unchecked(0x08010000)
-        },
-        // CPU interface register map goes from 0x0000-0x1000
-        // we only need 0x1004 bytes, but will request 0x2000
-        length: 0x00002000,
-        kind: MemoryRegionKind::Reserved,
-    };
-}
-
 use crate::memory::{MemoryRegion, MemoryRegionKind, PhysAddr};
 
 pub const DTB_ADDR: PhysAddr = unsafe {
