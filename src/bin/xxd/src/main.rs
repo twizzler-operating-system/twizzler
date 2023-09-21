5da3e197b839efb333f184390a0cbf68df8a74c5
CPTforever <yester1324@gmail.com>
2023-09-21 15:15:27 +0000

    Added color to xxd

--- src/bin/xxd/src/main.rs
+++ src/bin/xxd/src/main.rs
@@ -14,8 +14,18 @@ fn foo(ch: char) -> bool {
         return false;
     }
 }
+
+fn bar(ch: char) -> bool {
+    if (ch == '\t' || ch == '\n') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 fn main() {
-    let path = std::env::args().nth(3).expect("Path pls");
+    let path = std::env::args().nth(1).expect("Path pls");
 
     let mut f = File::open(&path).expect("Couldn't open file :(");
     let buf: &mut [u8; 16] = &mut [0; 16];
@@ -31,7 +41,18 @@ fn main() {
         let mut entry3 = String::new();
         for i in 0..16 {
             if i < x {
+                if foo(buf[i] as char) {
+                    entry2.push_str("\x1b[0;32m");
+                }
+                else if bar(buf[i] as char) {
+                    entry2.push_str("\x1b[0;33m");
+                }
+                else {
+                    entry2.push_str("\x1b[0;31m");
+                }
                 entry2.push_str(&format!("{:0>2x}", buf[i]));
+                entry2.push_str("\x1b[0m");
+
             }
             else {
                 entry2.push_str("  ");
@@ -41,12 +62,23 @@ fn main() {
 
         for i in 0..x {
             let x : char = buf[i] as char;
+            if foo(buf[i] as char) {
+                entry3.push_str("\x1b[0;32m");
+            }
+            else if bar(buf[i] as char) {
+                entry3.push_str("\x1b[0;33m");
+            }
+            else {
+                entry3.push_str("\x1b[0;31m");
+            }
+
             if foo(x) {
                 entry3.push(x); 
             }
             else {
                 entry3.push('.');
             }
+            entry3.push_str("\x1b[0m");
         }
 
         println!("{}: {} {}", entry1, entry2, entry3);
