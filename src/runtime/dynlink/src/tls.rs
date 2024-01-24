802cda24c16ca3a55072d78e1c78277868fad468
Deploy Actions <null@null.com>
2024-01-24 00:11:10 +0000

    Cleanup.

--- src/runtime/dynlink/src/tls.rs
+++ src/runtime/dynlink/src/tls.rs
@@ -189,12 +189,6 @@ impl<T> Tcb<T> {
     }
 
     pub fn get_addr(&self, index: &TlsIndex) -> Option<*const u8> {
-        /*
-        twizzler_abi::syscall::sys_kernel_console_write(
-            b"get_addr\n",
-            twizzler_abi::syscall::KernelConsoleWriteFlags::empty(),
-        );
-        */
         unsafe {
             let slice = core::slice::from_raw_parts(self.dtv, self.dtv_len);
             Some((slice.get(index.mod_id)? + index.offset) as *const _)
