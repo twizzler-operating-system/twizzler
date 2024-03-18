2ad14c453187369341f7cf90b1bea77d5c0c8c98
Esteban Ramos <esiramos@ucsc.edu>
2024-03-18 07:26:37 +0000

    wip thread verif

--- src/lib/twizzler-abi/src/thread.rs
+++ src/lib/twizzler-abi/src/thread.rs
@@ -96,36 +96,35 @@ impl From<ThreadSpawnError> for SpawnError {
 }
 
 
-// #[cfg(kani)]
+#[cfg(kani)]
+mod thread_verif{
+    use super::ExecutionState;
+    use super::ThreadRepr;
 
-// mod thread_verif{
-//     use super::ExecutionState;
-//     use super::ThreadRepr;
-
-//     #[kani::proof]
-//     pub fn thread_set_state(){
+    #[kani::proof]
+    pub fn fixme_thread_set_state(){
     
-//         let tr = ThreadRepr::default();
+        let tr = ThreadRepr::default();
         
-//         let state = exec_state();
+        let state = exec_state();
 
-//         tr.set_state(state, kani::any());
+        tr.set_state(state, kani::any());
         
-//         assert!(tr.get_state() == state);
+        assert!(tr.get_state() == state);
 
-//     }
+    }
 
-//     pub fn exec_state() -> ExecutionState{
-//         let state = match kani::any(){
-//                         0 => ExecutionState::Exited,
-//                         1 => ExecutionState::Sleeping, 
-//                         2 => ExecutionState::Running,
-//                         _ => ExecutionState::Suspended,
-//                     };
-//         return state;
-//     }
+    pub fn exec_state() -> ExecutionState{
+        let state = match kani::any(){
+                        0 => ExecutionState::Exited,
+                        1 => ExecutionState::Sleeping, 
+                        2 => ExecutionState::Running,
+                        _ => ExecutionState::Suspended,
+                    };
+        return state;
+    }
 
-// }
+}
 
 
 
