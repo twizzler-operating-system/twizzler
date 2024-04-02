d5e666454d48cd7c9e1fd0af5c63e56a7a4c7d74
Daniel Bittman <danielbittman1@gmail.com>
2024-04-02 22:49:38 +0000

    Merge.

---
+++ src/runtime/logboi/logboi-impl/src/lib.rs
@@ -0,0 +1,22 @@
+#![feature(naked_functions)]
+
+#[secgate::secure_gate]
+pub fn logboi_test(x: u32) -> u32 {
+    x + 1
+}
+
+#[used]
+#[doc(hidden)]
+#[allow(non_upper_case_globals)]
+#[link_section = ".init_array"]
+static ___cons_test___ctor: unsafe extern "C" fn() = {
+    #[allow(non_snake_case)]
+    #[link_section = ".text.startup"]
+    unsafe extern "C" fn ___cons_test___ctor() {
+        cons_test()
+    }
+    ___cons_test___ctor
+};
+unsafe fn cons_test() {
+    println!("LBI: CONS TEST");
+}
---
+++ src/runtime/logboi/src/lib.rs
@@ -0,0 +1,4 @@
+extern crate logboi_impl;
+secgate::secgate_prelude!();
+
+pub use logboi_impl::logboi_test;
