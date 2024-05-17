5f08453832369953dd9168ed331fee5c0913d5d2
Esteban Ramos <esiramos@ucsc.edu>
2024-05-17 20:11:04 +0000

    get rid of extra feature flag, just use kani cfg

--- src/kernel/src/memory/allocator.rs
+++ src/kernel/src/memory/allocator.rs
@@ -12,7 +12,7 @@ use crate::spinlock::Spinlock;
 use super::context::{Context, KernelMemoryContext};
 
 
-#[cfg(not(feature = "kani_exclude"))] 
+#[cfg(not(kani))]
 #[alloc_error_handler]
 fn alloc_error_handler(layout: Layout) -> ! {
     panic!("allocation error: {:?}", layout)
