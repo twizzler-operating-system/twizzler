3e29ca80d077f9bad0bc5044cb7c3a8b7c342057
Daniel Bittman <danielbittman1@gmail.com>
2024-07-02 11:01:48 -0700

    Initial api testing.

--- src/lib/twizzler/src/object/mod.rs
+++ src/lib/twizzler/src/object/mod.rs
@@ -2,6 +2,8 @@ use std::marker::PhantomData;
 
 use twizzler_runtime_api::{MapError, MapFlags, ObjID, ObjectHandle};
 
+use crate::tx::{TxHandle, TxResult};
+
 pub struct Object<Base: BaseType> {
     handle: ObjectHandle,
     _pd: PhantomData<*const Base>,
@@ -13,6 +15,13 @@ impl<Base: BaseType> Object<Base> {
     }
 
     pub fn open(&self, id: ObjID, flags: MapFlags) -> Result<Self, MapError> {
+        todo!()
+    }
+
+    pub fn tx<TxFn, Ret, Err>(&self, txfn: TxFn) -> TxResult<Ret, Err>
+    where
+        TxFn: FnOnce(TxHandle<'_>) -> Result<Ret, Err>,
+    {
         todo!()
     }
 }
