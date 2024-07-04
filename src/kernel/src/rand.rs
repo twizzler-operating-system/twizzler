38f1d19e1c2fcb582331a625e7a6c3d75e9a4304
zphrs <zejones@ucsc.edu>
2024-07-04 17:32:42 +0000

    refined cfg flags

--- src/kernel/src/rand.rs
+++ src/kernel/src/rand.rs
@@ -3,13 +3,13 @@ use core::arch::asm;
 use rand_chacha::ChaCha20Rng;
 use rdrand::RdSeed;
 
-#[cfg(target_feature = "rdrand")]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub fn get_random_seed() -> Option<u64> {
     let s = RdSeed::new().unwrap();
     s.try_next_u64().ok()
 }
 
-#[cfg(target_arch = "aarch64")]
+#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
 pub fn get_random_seed_arch() -> Option<u64> {
     // https://github.com/CTSRD-CHERI/cheribsd/blob/bdeff30fb6b1744816f43ed8a3c2f0a133d872c1/sys/dev/random/armv8rng.c#L54-L73
     todo!();
