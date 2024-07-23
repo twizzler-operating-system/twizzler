c1fcd130afd1863dff44e57b55da9d29a8f50790
Daniel Bittman <danielbittman1@gmail.com>
2024-07-23 14:55:04 -0700

    Working on pbox.

--- src/lib/twizzler/src/alloc/pbox.rs
+++ src/lib/twizzler/src/alloc/pbox.rs
@@ -1,7 +1,12 @@
-use std::alloc::{AllocError, Layout};
+use std::{
+    alloc::{AllocError, Layout},
+    marker::PhantomData,
+    ops::Deref,
+};
 
 use super::Allocator;
 use crate::{
+    marker::{InPlace, StoreEffect},
     object::InitializedObject,
     ptr::{GlobalPtr, InvPtr, InvPtrBuilder},
     tx::{TxHandle, TxResult},
@@ -9,31 +14,86 @@ use crate::{
 
 #[derive(twizzler_derive::Invariant)]
 #[repr(C)]
-pub struct PBox<T> {
+pub struct PBox<T, A: Allocator> {
     ptr: InvPtr<T>,
+    alloc: A,
 }
 
-pub struct PBoxBuilder<T> {
+pub struct PBoxBuilder<T, A: Allocator> {
     inv: InvPtrBuilder<T>,
+    alloc: A,
 }
 
-impl<T> std::ops::Deref for PBox<T> {
-    type Target = T;
+impl<T, A: Allocator> Deref for PBox<T, A> {
+    type Target = InvPtr<T>;
 
     fn deref(&self) -> &Self::Target {
-        todo!()
+        &self.ptr
     }
 }
 
-impl<T> PBox<T> {
-    pub fn new_in<A: Allocator>(value: T, alloc: &A) -> Result<PBoxBuilder<T>, AllocError> {
-        todo!()
+impl<T, A: Allocator> PBox<T, A> {
+    pub unsafe fn from_invptr(ptr: InvPtr<T>, alloc: A) -> Self {
+        Self { ptr, alloc }
+    }
+
+    pub fn new(value: T, alloc: A) -> Result<PBoxBuilder<T, A>, AllocError> {
+        let gptr = alloc.allocate(Layout::new::<T>())?.cast::<T>();
+        let ptr = gptr.resolve().map_err(|_| AllocError)?;
+        let mut mut_ptr = unsafe { ptr.as_mut() };
+        *mut_ptr = value;
+
+        Ok(PBoxBuilder {
+            inv: unsafe { InvPtrBuilder::from_global(gptr) },
+            alloc,
+        })
+    }
+
+    pub fn new_with(
+        ctor: impl FnOnce(InPlace) -> T,
+        alloc: A,
+    ) -> Result<PBoxBuilder<T, A>, AllocError> {
+        let gptr = alloc.allocate(Layout::new::<T>())?.cast::<T>();
+        let ptr = gptr.resolve().map_err(|_| AllocError)?;
+        let mut mut_ptr = unsafe { ptr.as_mut() };
+        let in_place = InPlace::new(&ptr.handle());
+        *mut_ptr = ctor(in_place);
+
+        Ok(PBoxBuilder {
+            inv: unsafe { InvPtrBuilder::from_global(gptr) },
+            alloc,
+        })
     }
 }
 
-impl<T> Drop for PBox<T> {
+impl<T, A: Allocator> Drop for PBox<T, A> {
     fn drop(&mut self) {
-        todo!()
+        if let Ok(res) = self.ptr.resolve() {
+            unsafe {
+                let ptr = res.ptr() as *mut T;
+                core::ptr::drop_in_place(ptr);
+            }
+        } else {
+            //TODO
+        }
+
+        if let Ok(res) = self.ptr.as_global() {
+            // TODO
+            let _ = unsafe { self.alloc.deallocate(res.cast(), Layout::new::<T>()) };
+        } else {
+            //TODO
+        }
+    }
+}
+
+impl<T, A: Allocator> StoreEffect for PBox<T, A> {
+    type MoveCtor = PBoxBuilder<T, A>;
+
+    fn store<'a>(ctor: Self::MoveCtor, in_place: &mut InPlace<'a>) -> Self
+    where
+        Self: Sized,
+    {
+        unsafe { PBox::from_invptr(in_place.store(ctor.inv), ctor.alloc) }
     }
 }
 
@@ -45,7 +105,10 @@ mod test {
 
     use super::{PBox, PBoxBuilder};
     use crate::{
-        alloc::{arena::ArenaManifest, TxAllocator},
+        alloc::{
+            arena::{ArenaAllocator, ArenaManifest},
+            TxAllocator,
+        },
         object::{BaseType, ConstructorInfo, InitializedObject, Object, ObjectBuilder},
         ptr::InvPtrBuilder,
         tx::{TxCell, TxHandle},
@@ -54,73 +117,27 @@ mod test {
     #[derive(twizzler_derive::Invariant)]
     #[repr(C)]
     struct Foo {
-        data: TxCell<PBox<u32>>,
+        data: PBox<Bar, ArenaAllocator>,
         data2: TxCell<u32>,
     }
 
     #[derive(twizzler_derive::Invariant, Copy, Clone)]
     #[repr(C)]
-    struct Foo2 {
+    struct Bar {
         x: u32,
     }
 
-    /*
-    unsafe impl twizzler::marker::Invariant for Foo {}
-    unsafe impl twizzler::marker::InPlaceCtor for Foo {
-        type Builder = FooBuilder;
-        fn in_place_ctor<'b>(
-            builder: Self::Builder,
-            place: &'b mut core::mem::MaybeUninit<Self>,
-            tx: impl twizzler::tx::TxHandle<'b>,
-        ) -> &'b mut Self
-        where
-            Self: Sized,
-        {
-            unsafe {
-                let ptr = place as *mut _ as *mut Self;
-                let ptr = addr_of!((*ptr).data) as *mut core::mem::MaybeUninit<TxCell<PBox<u32>>>;
-                <TxCell<PBox<u32>>>::in_place_ctor(builder.data, &mut *ptr, &tx);
-            }
-            unsafe {
-                let ptr = place as *mut _ as *mut Self;
-                let ptr = addr_of!((*ptr).data2) as *mut core::mem::MaybeUninit<TxCell<u32>>;
-                <TxCell<u32>>::in_place_ctor(builder.data2, &mut *ptr, &tx);
-            }
-            unsafe { place.assume_init_mut() }
-        }
-    }
-    struct FooBuilder {
-        data: <TxCell<PBox<u32>> as InPlaceCtor>::Builder, // PBoxBuilder
-        data2: <TxCell<u32> as InPlaceCtor>::Builder, // u32
-    }
-    impl Foo {
-        pub fn new(
-            data: <TxCell<PBox<u32>> as InPlaceCtor>::Builder,
-            data2: <TxCell<u32> as InPlaceCtor>::Builder,
-        ) -> FooBuilder {
-            FooBuilder { data, data2 }
-        }
-    }
-
-    impl BaseType for Foo {}
-    impl BaseType for Foo2 {}
-
-    fn test<'a>(alloc: Object<ArenaManifest>, tx: impl TxHandle<'a>) {
-        let obj: Object<Foo> = ObjectBuilder::default()
-            .construct(|_info| Ok(Foo::new(PBox::new_in(32, &alloc).unwrap(), 334)))
+    fn test() {
+        let obj = ObjectBuilder::default()
+            .init(ArenaManifest::default())
             .unwrap();
+        let arena = obj.base();
 
-        let base = obj.base();
-        base.data
-            .set_in_place(PBox::new_in(64, &alloc).unwrap(), &tx)
+        let foo = arena
+            .alloc_with(|mut ip| Foo {
+                data: ip.store(PBox::new(Bar { x: 42 }, arena).unwrap()),
+                data2: TxCell::new(3),
+            })
             .unwrap();
-        let _data = **base.data;
-
-        base.data2.set(42, tx).unwrap();
-
-        let o2 = ObjectBuilder::default().init(Foo2 { x: 4 }).unwrap();
-        let base = o2.base();
-        let _data = base.x;
     }
-    */
 }
