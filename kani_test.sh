8e6b294a1e0db00aa7ec17f4bdccd69a8a2bf079
Alexander <awaghili@ucsc.edu>
2024-07-31 20:35:46 +0000

    github actions test

--- kani_test.sh
+++ kani_test.sh
@@ -2,4 +2,4 @@
 cd kani
 cargo build-dev
 cd ..
-./kani/scripts/cargo-kani --workspace --exclude monitor -Z stubbing
+./kani/scripts/cargo-kani --workspace --exclude monitor -Z stubbing  --output-into-files
