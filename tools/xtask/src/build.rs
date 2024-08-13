c35c2af933fb14eb87cb11337e5743cb819ed86e
Daniel Bittman <danielbittman1@gmail.com>
2024-08-13 16:01:46 -0700

    Added test for simple buffer.

--- tools/xtask/src/build.rs
+++ tools/xtask/src/build.rs
@@ -285,7 +285,6 @@ fn maybe_build_tests<'a>(
                 "twz-rt" => None,
                 "monitor" => None,
                 "bootstrap" => None,
-                "secgate" => None,
                 "secgate-macros" => None,
                 _ => Some(p.name().to_string()),
             })
