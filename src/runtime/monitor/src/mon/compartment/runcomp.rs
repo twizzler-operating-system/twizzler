c97459b557bc0eebace4cd35ff4eb3f1a6f03221
Daniel Bittman <danielbittman1@gmail.com>
2024-08-20 16:30:46 -0700

    Security montitor (1) (#199)
    * Initial compartment support.
    
    * Initial support for threads and addr space management.

---
+++ src/runtime/monitor/src/mon/compartment/runcomp.rs
@@ -0,0 +1,156 @@
+use std::{
+    alloc::Layout,
+    collections::HashMap,
+    marker::PhantomData,
+    ptr::NonNull,
+    sync::atomic::{AtomicU64, Ordering},
+};
+
+use dynlink::compartment::CompartmentId;
+use monitor_api::SharedCompConfig;
+use talc::{ErrOnOom, Talc};
+use twizzler_abi::syscall::{
+    ThreadSync, ThreadSyncFlags, ThreadSyncOp, ThreadSyncReference, ThreadSyncSleep, ThreadSyncWake,
+};
+use twizzler_runtime_api::{MapError, ObjID};
+
+use super::{compconfig::CompConfigObject, compthread::CompThread};
+use crate::mon::space::{MapHandle, MapInfo};
+
+/// Compartment is ready (loaded, reloacated, runtime started and ctors run).
+pub const COMP_READY: u64 = 0x1;
+/// Compartment is a binary, not a library.
+pub const COMP_IS_BINARY: u64 = 0x2;
+/// Compartment runtime thread may exit.
+pub const COMP_THREAD_CAN_EXIT: u64 = 0x4;
+
+/// A runnable or running compartment.
+pub struct RunComp {
+    /// The security context for this compartment.
+    pub sctx: ObjID,
+    /// The instance of the security context.
+    pub instance: ObjID,
+    /// The name of this compartment.
+    pub name: String,
+    /// The dynlink ID of this compartment.
+    pub compartment_id: CompartmentId,
+    main: Option<CompThread>,
+    deps: Vec<ObjID>,
+    comp_config_object: CompConfigObject,
+    alloc: Talc<ErrOnOom>,
+    mapped_objects: HashMap<MapInfo, MapHandle>,
+    flags: Box<AtomicU64>,
+}
+
+impl RunComp {
+    /// Map an object into this compartment.
+    pub fn map_object(&mut self, info: MapInfo) -> Result<MapHandle, MapError> {
+        todo!()
+    }
+
+    /// Unmap and object from this compartment.
+    pub fn unmap_object(&mut self, info: MapInfo) {
+        let _ = self.mapped_objects.remove(&info);
+        // Unmapping handled by dropping
+    }
+
+    /// Read the compartment config.
+    pub fn comp_config(&self) -> SharedCompConfig {
+        self.comp_config_object.read_comp_config()
+    }
+
+    /// Set the compartment config.
+    pub fn set_comp_config(&mut self, scc: SharedCompConfig) {
+        self.comp_config_object.write_config(scc)
+    }
+
+    /// Allocate some space in the compartment allocator, and initialize it.
+    pub fn monitor_new<T: Copy + Sized + Send + Sync>(&mut self, data: T) -> Result<*mut T, ()> {
+        unsafe {
+            let place: NonNull<T> = self.alloc.malloc(Layout::new::<T>())?.cast();
+            place.as_ptr().write(data);
+            Ok(place.as_ptr() as *mut T)
+        }
+    }
+
+    /// Allocate some space in the compartment allocator for a slice, and initialize it.
+    pub fn monitor_new_slice<T: Copy + Sized + Send + Sync>(
+        &mut self,
+        data: &[T],
+    ) -> Result<*mut T, ()> {
+        unsafe {
+            let place = self.alloc.malloc(Layout::array::<T>(data.len()).unwrap())?;
+            let slice = core::slice::from_raw_parts_mut(place.as_ptr() as *mut T, data.len());
+            slice.copy_from_slice(data);
+            Ok(place.as_ptr() as *mut T)
+        }
+    }
+
+    /// Set a flag on this compartment, and wakeup anyone waiting on flag change.
+    pub fn set_flag(&self, val: u64) {
+        self.flags.fetch_or(val, Ordering::SeqCst);
+        let _ = twizzler_abi::syscall::sys_thread_sync(
+            &mut [ThreadSync::new_wake(ThreadSyncWake::new(
+                ThreadSyncReference::Virtual(&*self.flags),
+                usize::MAX,
+            ))],
+            None,
+        );
+    }
+
+    /// Check if a flag is set.
+    pub fn has_flag(&self, flag: u64) -> bool {
+        self.flags.load(Ordering::SeqCst) & flag != 0
+    }
+
+    /// Setup a [ThreadSyncSleep] for waiting until the flag is set. Returns None if the flag is
+    /// already set.
+    pub fn flag_waitable(&self, flag: u64) -> Option<ThreadSyncSleep> {
+        let flags = self.flags.load(Ordering::SeqCst);
+        if flags & flag == 0 {
+            Some(ThreadSyncSleep::new(
+                ThreadSyncReference::Virtual(&*self.flags),
+                flags,
+                ThreadSyncOp::Equal,
+                ThreadSyncFlags::empty(),
+            ))
+        } else {
+            None
+        }
+    }
+
+    /// Return a waiter for this flag, allows calling .wait later to wait until a flag is set.
+    pub fn waiter(&self, flag: u64) -> RunCompReadyWaiter<'_> {
+        RunCompReadyWaiter {
+            flag,
+            sleep: self.flag_waitable(flag),
+            _pd: PhantomData,
+        }
+    }
+}
+
+/// Allows waiting for a compartment to set a flag, sleeping the calling thread until the flag is
+/// set.
+pub struct RunCompReadyWaiter<'a> {
+    flag: u64,
+    sleep: Option<ThreadSyncSleep>,
+    _pd: PhantomData<&'a ()>,
+}
+
+impl<'a> RunCompReadyWaiter<'a> {
+    /// Wait until the compartment is marked as ready.
+    pub fn wait(&self) {
+        loop {
+            let Some(sleep) = self.sleep else { return };
+            if sleep.ready() {
+                return;
+            }
+
+            if let Err(e) =
+                twizzler_abi::syscall::sys_thread_sync(&mut [ThreadSync::new_sleep(sleep)], None)
+            {
+                tracing::warn!("thread sync error: {:?}", e);
+            }
+        }
+    }
+}
--- src/runtime/monitor/src/mon/space/unmapper.rs
+++ src/runtime/monitor/src/mon/space/unmapper.rs
@@ -3,40 +3,48 @@ use std::{panic::catch_unwind, sync::mpsc::Sender, thread::JoinHandle};
 use super::MapInfo;
 use crate::mon::get_monitor;
 
+/// Manages a background thread that unmaps mappings.
 pub struct Unmapper {
     sender: Sender<MapInfo>,
     thread: JoinHandle<()>,
 }
 
 impl Unmapper {
-    pub fn new(cb: fn(MapInfo)) -> Self {
+    /// Make a new unmapper.
+    pub fn new() -> Self {
         let (sender, receiver) = std::sync::mpsc::channel();
         Self {
-            thread: std::thread::spawn(move || loop {
-                let key = happylock::ThreadKey::get().unwrap();
-                match receiver.recv() {
-                    Ok(info) => {
-                        if let Err(_) = catch_unwind(|| {
-                            let monitor = get_monitor();
-                            let mut space = monitor.space.write(key);
-                            space.handle_drop(info);
-                        }) {
-                            tracing::error!("clean_call panicked -- exiting map cleaner thread");
+            thread: std::thread::Builder::new()
+                .name("unmapper".to_string())
+                .spawn(move || loop {
+                    let key = happylock::ThreadKey::get().unwrap();
+                    match receiver.recv() {
+                        Ok(info) => {
+                            if let Err(_) = catch_unwind(|| {
+                                let monitor = get_monitor();
+                                let mut space = monitor.space.write(key);
+                                space.handle_drop(info);
+                            }) {
+                                tracing::error!(
+                                    "clean_call panicked -- exiting map cleaner thread"
+                                );
+                                break;
+                            }
+                        }
+                        Err(_) => {
+                            // If receive fails, we can't recover, but this probably doesn't happen
+                            // since the sender won't get dropped since this
+                            // struct is used in the MapMan static.
                             break;
                         }
                     }
-                    Err(_) => {
-                        // If receive fails, we can't recover, but this probably doesn't happen
-                        // since the sender won't get dropped since this
-                        // struct is used in the MapMan static.
-                        break;
-                    }
-                }
-            }),
+                })
+                .unwrap(),
             sender,
         }
     }
 
+    /// Enqueue a mapping to be unmapped.
     pub(super) fn background_unmap_info(&self, info: MapInfo) {
         // If the receiver is down, this will fail, but that also shouldn't happen, unless the
         // call to clean_call above panics. In any case, handle this gracefully.
