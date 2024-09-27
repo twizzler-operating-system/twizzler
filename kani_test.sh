0cd7c959bdf64e40878f30b794b740f9f7e6efeb
Alexander <awaghili@ucsc.edu>
2024-09-27 21:43:03 +0000

    Leftover non merge

--- kani_test.sh
+++ kani_test.sh
@@ -2,8 +2,4 @@
 cd kani
 cargo build-dev
 cd ..
-<<<<<<< HEAD
 ./kani/scripts/cargo-kani --workspace --exclude monitor -Z stubbing -Z unstable-options  --output-into-files
-=======
-./kani/scripts/cargo-kani --workspace --exclude monitor -Z stubbing  --output-into-files
->>>>>>> 73de36adf36e949d259f1388d0743ca73c227ec3
