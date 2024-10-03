53272b61e95f753d8f380f0267e651e2e2693a22
Alexander <awaghili@ucsc.edu>
2024-10-03 19:59:48 +0000

    Fixed rebase errors

--- src/lib/twizzler-abi/src/object.rs
+++ src/lib/twizzler-abi/src/object.rs
@@ -1,23 +1,16 @@
 //! Low-level object APIs, mostly around IDs and basic things like protection definitions and metadata.
 
-<<<<<<< HEAD
 use core::{
     fmt::{LowerHex, UpperHex},
     marker::PhantomData,
 };
 
-=======
->>>>>>> 73de36adf36e949d259f1388d0743ca73c227ec3
 /*
 KANI_TODO
 */
 
-<<<<<<< HEAD
 
 use crate::syscall::{MapFlags, ObjectCreate, ObjectCreateFlags};
-=======
-use core::fmt::{LowerHex, UpperHex};
->>>>>>> 73de36adf36e949d259f1388d0743ca73c227ec3
 
 /// The maximum size of an object, including null page and meta page(s).
 pub const MAX_SIZE: usize = 1024 * 1024 * 1024;
