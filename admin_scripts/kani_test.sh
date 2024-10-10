a89afcb87dfc01de528e79b9fd3d8c15f0c515d4
Alexander <awaghili@ucsc.edu>
2024-10-10 18:21:28 +0000

    Working update

--- admin_scripts/kani_test.sh
+++ admin_scripts/kani_test.sh
@@ -2,5 +2,4 @@
 cd ~/kani 
 cargo build-dev
 cd ~/twizzler
-../kani/scripts/cargo-kani --workspace --exclude monitor -Z stubbing -Z unstable-options --output-into-files
-
+../kani/scripts/cargo-kani --workspace --exclude monitor --exclude twizzler-kernel -Z stubbing -Z unstable-options --output-into-files
