7f1bd3ba6ac23b5ddf527b3d2fab1c6c1cdd21de
Daniel Bittman <danielbittman1@gmail.com>
2024-11-08 00:08:42 +0000

    Don't stall in TLB shootdown.
    This is an artifact of a sub-par TLB shootdown algorithm that needs to
    be updated. That'll be a future PR, in the meantime, this removes a
    possible deadlock.

--- src/kernel/src/arch/amd64/memory/pagetables/consistency.rs
+++ src/kernel/src/arch/amd64/memory/pagetables/consistency.rs
@@ -311,18 +311,31 @@ impl ArchTlbMgr {
         self.data.do_invalidation();
 
         if count > 0 {
+            // Ensure we don't wait too long -- TODO: this is because this TLB shootdown algorithm
+            // is Not Great (tm) and should be improved (targeted shootdown, pcid tracking, ...)
+            const MAX_ITERS: usize = 100;
             // Wait for each processor to report that it is done.
             with_each_active_processor(|p| {
+                let mut iters = 0;
                 if p.id != proc.id {
                     spin_wait_until(
                         || {
-                            if p.arch.tlb_shootdown_info.is_finished() {
+                            if p.arch.tlb_shootdown_info.is_finished() || iters >= MAX_ITERS {
+                                if iters >= MAX_ITERS {
+                                    logln!(
+                                        "warning -- TLB shootdown pause on CPUs {} -> {}",
+                                        proc.id,
+                                        p.id
+                                    );
+                                }
                                 Some(())
                             } else {
                                 None
                             }
                         },
-                        || {},
+                        || {
+                            iters += 1;
+                        },
                     );
                 }
             });
