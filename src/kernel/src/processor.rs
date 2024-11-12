58f5c7c52882b3a12db1cc70fd8f5a00c6a25d04
Daniel Bittman <danielbittman1@gmail.com>
2024-11-12 22:45:30 +0000

    Increase kernel stack size.

--- src/kernel/src/processor.rs
+++ src/kernel/src/processor.rs
@@ -299,7 +299,7 @@ pub fn tls_ready() -> bool {
     crate::arch::processor::tls_ready()
 }
 
-pub const KERNEL_STACK_SIZE: usize = 81920;
+pub const KERNEL_STACK_SIZE: usize = 1 * 512 * 1024; // 1MB
 
 pub fn init_cpu(tls_template: TlsInfo, bsp_id: u32) {
     let tcb_base = crate::arch::image::init_tls(tls_template);
