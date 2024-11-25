74b28626e99c8ed4ca22bf59710b746c0931198f
Thomas Dillow <tdillow@ucsc.edu>
2024-11-25 13:58:33 -0800

    progress from last week before updating from main

--- src/bin/virtio/src/hal.rs
+++ src/bin/virtio/src/hal.rs
@@ -1,13 +1,16 @@
-use core::ptr::NonNull;
-
+use twizzler_abi::syscall::{BackingType, LifetimeType};
+use twizzler_abi::marker::{BaseType, BaseVersion, BaseTag};
 use twizzler_driver::dma::{Access, DmaOptions, DmaPool, DmaSliceRegion, DMA_PAGE_SIZE};
+use twizzler_object::{CreateSpec, Object};
 
 use virtio_drivers::{BufferDirection, Hal, PhysAddr};
 
 use once_cell::sync::OnceCell; 
-use std::{collections::HashMap, ptr::slice_from_raw_parts};
+use std::cell::UnsafeCell;
+use std::collections::HashMap;
 use fragile::Fragile;
 use std::sync::Mutex;
+use core::ptr::NonNull;
 
 pub struct TestHal;
 
@@ -15,7 +18,27 @@ static DMA_POOL_HOST_TO_DEVICE: OnceCell<DmaPool> = OnceCell::new();
 static DMA_POOL_DEVICE_TO_HOST: OnceCell<DmaPool> = OnceCell::new();
 static DMA_POOL_BIDIRECTIONAL: OnceCell<DmaPool> = OnceCell::new();
 
+// The DmaSliceRegions contained within this hashmap are never operated upon after being inserted into this hashmap, only the memory beneath it is.
+// This hashmap is used to keep memory allocated while it is still in use.
 static ALLOCED: OnceCell<Mutex<HashMap<PhysAddr, Fragile<DmaSliceRegion<u8>>>>> = OnceCell::new();
+
+static BUFFERS: OnceCell<Mutex<HashMap<PhysAddr, Object<BufferWrapper>>>> = OnceCell::new();
+
+struct BufferWrapper {
+    buffer: NonNull<[u8]>
+}
+
+unsafe impl Send for BufferWrapper {}
+
+impl BaseType for BufferWrapper {
+    fn init<T>(_t: T) -> Self {
+        todo!()
+    }
+
+    fn tags() -> &'static [(BaseVersion, BaseTag)] {
+        todo!()
+    }
+}
 
 // Gets the global dma pool for the HAL in a given access direction. If it doesn't exist, create it.
 fn get_dma_pool(dir: BufferDirection) -> &'static DmaPool {
@@ -96,7 +119,6 @@ unsafe impl Hal for TestHal{
         let alloced = get_dma_pool(direction).allocate_array(pages * DMA_PAGE_SIZE, 0u8);
 
         let mut dma_slice = alloced.unwrap();
-        println!("Allocated DMA buffer of size: {}", dma_slice.num_bytes());
         let pin = dma_slice.pin().unwrap();
         
         let phys_addr: virtio_drivers::PhysAddr =
@@ -122,25 +144,34 @@ unsafe impl Hal for TestHal{
     }
 
     unsafe fn share(buffer: NonNull<[u8]>, _direction: BufferDirection) -> PhysAddr {
-        // Create a new buffer for it to work in in DMA memory so it can be pinned
-        let pool = get_dma_pool(_direction);
-        let mut dma_slice = pool.allocate_array(buffer.len(), 0u8).unwrap();
+        // let vaddr = buffer.as_ptr() as *mut u8 as usize;
+        // println!("Sharing buffer at {:?}", buffer);
+        // vaddr as PhysAddr
+        
+        let create_spec = &CreateSpec::new(LifetimeType::Volatile, BackingType::Normal);
+        
+        let obj: Object<BufferWrapper> = Object::create_with(create_spec, |obj| unsafe {
+            let base: &mut BufferWrapper = obj.base_mut_unchecked().assume_init_mut();
+            base.buffer.copy_from(buffer, buffer.len());
+        }).unwrap(); 
 
-        let pin = dma_slice.pin().unwrap();
-
-        let paddr: virtio_drivers::PhysAddr = u64::from(pin.into_iter().next().unwrap().addr()) as virtio_drivers::PhysAddr;
-        let ptr = unsafe{dma_slice.get_mut().as_mut_ptr()};
 
 
-        // Persist the allocated memory so it isn't freed when the function returns
-        insert_alloced(paddr, dma_slice);
-
-        // Copy the data from the passed in buffer into the newly created one
-        let const_buf: *const [u8] = &buffer.as_ref()[0..buffer.len()];
-        let ptr_slice = slice_from_raw_parts(ptr, buffer.len());
-        unsafe {const_buf.copy_to(ptr_slice, buffer.len());}
-
-        paddr
+        // Pin object and get phys address
+        
+        
+        // Hold onto the object until it's unshared.
+        let dict = BUFFERS.get_or_init(|| Mutex::new(HashMap::new()));
+        todo!();
     }
-    unsafe fn unshare(_paddr: PhysAddr, _buffer: NonNull<[u8]>, _direction: BufferDirection) {}
+    unsafe fn unshare(_paddr: PhysAddr, _buffer: NonNull<[u8]>, _direction: BufferDirection) {
+        todo!();
+        //Find the associated object
+        
+        
+        // Copy the buffer back to the original buffer
+
+
+        // Unpin object
+    }
 }
