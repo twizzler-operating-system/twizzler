f62cabcd6b4fa11be65a6f288588e3521f2cbf9e
Alexander Aghili <alexander.w.aghili@gmail.com>
2024-12-26 18:04:04 -0800

    Updated to add a Test request that performs kernel request

--- src/lib/twizzler-abi/src/pager.rs
+++ src/lib/twizzler-abi/src/pager.rs
@@ -39,7 +39,7 @@ impl CompletionToKernel {
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum KernelCompletionData {
     EchoResp,
-    PageDataReq(PhysRange)
+    PageDataCompletion(PhysRange)
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Ord, Eq)]
