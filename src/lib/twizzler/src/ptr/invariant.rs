4683e39f7cb76b651801b3a4d0ddb30c3d8dd86c
Daniel Bittman <danielbittman1@gmail.com>
2024-12-30 12:27:44 -0800

    Twizzler crate stubs for transactions and pointers. (#226)
    * Adding store effects.
    
    * Adding transactional stubs.

--- src/lib/twizzler/src/ptr/invariant.rs
+++ src/lib/twizzler/src/ptr/invariant.rs
@@ -1,6 +1,10 @@
 use std::marker::PhantomData;
 
-use crate::marker::{Invariant, PhantomStoreEffect};
+use super::{GlobalPtr, Ref};
+use crate::{
+    marker::{Invariant, PhantomStoreEffect, Storable},
+    tx::TxHandle,
+};
 
 #[repr(C)]
 #[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
@@ -8,4 +12,14 @@ pub struct InvPtr<T: Invariant> {
     value: u64,
     _pse: PhantomStoreEffect,
     _pd: PhantomData<*const T>,
+}
+
+impl<T: Invariant> InvPtr<T> {
+    pub fn new_in(target: &impl TxHandle, global: impl Into<GlobalPtr<T>>) -> Storable<Self> {
+        todo!()
+    }
+
+    pub fn resolve<'a>(&self) -> Ref<'a, T> {
+        todo!()
+    }
 }
