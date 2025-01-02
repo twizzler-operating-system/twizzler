72babc2294cc74e04d6ad7c2ff6cb92f6fec07e9
Zephiris <z@zephiris.dev>
2025-01-02 14:24:25 -0800

    Zphrs random two (#231)
    * changed gitmodules
    
    * made gitmodules url relative
    
    * init new branch
    
    * WIP on randomness
    
    * updated kernel randomness to work with new abi system.
    
    * fixed up demo, removed unnecessary print statements
    
    * added random_validation back
    
    * added genrandom back
    
    * fixed gitmodules
    
    * fixed submodule
    
    * fixed gitmodules again
    
    * fixed gitmodules again
    
    * fixed up conditional twizzler statement
    
    * Removed deadlock comment.
    
    * removed the submodules getrandom and twizzler-rt-abi and instead used a crates-io patch.
    
    * changed getrandom patch to point to twizzler-operating-system/getrandom-twizzler; removed unnecessary gitmodules
    
    * Fixed cargo.lock conflicts
    
    * Added the submodule back in
    
    * reverted submodules to twizzler-os
    
    * src/abi
    
    * switched back to using my personal getrandom branch for now, will switch back once merged
    
    * switched back getrandom to twizzler fork
    
    * removed unnecessary print statement
    
    * src/abi gitmodule
    
    * fixed merge mistakes

---
+++ src/bin/random_validation/src/diehardest/mod.rs
@@ -0,0 +1,51 @@
+pub mod analysis;
+pub mod transform;
+
+/// A random number generator.
+pub trait Random {
+    /// Get a random number.
+    fn get_random(&mut self) -> u64;
+}
+
+/// 13 tests in total implemented
+const TOTAL_TESTS: u32 = 13;
+pub const EXPECTED_SCORE: u32 = TOTAL_TESTS * 1024;
+pub fn crush<R: Random + Clone>(rand: R) -> u32 {
+    analysis::Report::new(rand.clone()).get_score().total() as u32
+        + analysis::Report::new(transform::SkipOne(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::SkipTwo(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::Concatenate32(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::Xor(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::Add(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::Multiply(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::LastBit(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::MultiplyByThree(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::ModularDivideByThree(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::Hamming(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::ParitySkip(rand.clone()))
+            .get_score()
+            .total() as u32
+        + analysis::Report::new(transform::Rol7(rand.clone()))
+            .get_score()
+            .total() as u32
+}
