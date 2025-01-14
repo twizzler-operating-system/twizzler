bf2b2990128fcecfe8b98eb5bbb256b640d71d6d
zphrs <z@zephiris.dev>
2025-01-13 19:13:43 -0800

    removed legacy directories

--- src/srv/pager-srv/object-store-old/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod disk;
-mod nvme;
-mod object_store;
-
-pub use object_store::{create_object, read_exact, unlink_object, write_all};
--- src/srv/pager-srv/obliviate/obliviate-core/src/kms/khf/khf_tree.rs
+++
@@ -1,9 +0,0 @@
-//! To display the KHF tree for debugging/showing off purposes.
-// TODO
-use std::collections::HashMap;
-
-use super::{node::Node, Pos};
-
-struct KhfTree<H, const N: usize> {
-    levels: Vec<HashMap<Pos, Node<H, N>>>,
-}
