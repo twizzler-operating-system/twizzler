a088356c8c1fcae2e2fb7f74c12c1660b909e413
suri312006 <suri312006@gmail.com>
2025-05-26 09:26:06 -0700

    fix debug impl

--- src/lib/twizzler-security/src/keys/sig.rs
+++ src/lib/twizzler-security/src/keys/sig.rs
@@ -27,7 +27,8 @@ impl Debug for Signature {
         f.debug_struct("Signature")
             .field("bytes", &self.buf[self.len])
             .field("len", &self.len)
-            .field("scheme", &self.scheme)
+            .field("scheme", &self.scheme)?;
+        Ok(())
     }
 }
 
