1cf32243356b58c856d6990a93723ff47f1596e6
suri312006 <suri312006@gmail.com>
2025-05-25 18:50:50 -0700

    please

--- src/kernel/src/utils.rs
+++ src/kernel/src/utils.rs
@@ -153,7 +153,7 @@ where
         let end = clock.read();
 
         // NOTE: times are in nanos
-        times.push((end.value - start.value) * end.rate);
+        times.push(((end.value - start.value) * end.rate).as_nanos());
     }
 
     let total_ns: u64 = times.iter().sum();
