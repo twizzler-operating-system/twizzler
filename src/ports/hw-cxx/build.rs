9d5844f550129da444c14a6739b86f64a0de0c09
Daniel Bittman <danielbittman1@gmail.com>
2025-07-10 15:31:37 -0700

    Support libc++.

--- src/ports/hw-cxx/build.rs
+++ src/ports/hw-cxx/build.rs
@@ -1,3 +1,12 @@
 fn main() {
-    cc::Build::new().file("src/test.cpp").compile("cxxtest");
+    cc::Build::new()
+        .cpp(true)
+        .compiler("clang++")
+        .file("src/test.cpp")
+        .cpp_set_stdlib("c++")
+        .compile("cxxtest");
+
+    println!("cargo::rustc-link-lib=c");
+    println!("cargo::rustc-link-lib=static=c++abi");
+    println!("cargo::rustc-link-lib=unwind");
 }
