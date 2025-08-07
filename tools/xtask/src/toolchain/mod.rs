2dbb2fb79e93816c87ecab62b02fb80418871a43
Surendra Jammishetti <suri312006@gmail.com>
2025-08-07 10:38:07 -0700

    put raj's changes back in

--- tools/xtask/src/toolchain/mod.rs
+++ tools/xtask/src/toolchain/mod.rs
@@ -71,8 +71,12 @@ pub fn handle_cli(subcommand: ToolchainCommands) -> anyhow::Result<()> {
     match subcommand {
         ToolchainCommands::Bootstrap(opts) => do_bootstrap(opts),
         // FIXME: commenting this out to fix build because its a mess rn
-        // ToolchainCommands::Pull => tokio::runtime::Runtime::new()?.block_on(pull_toolchain()),
-        ToolchainCommands::Pull => Ok(()),
+        ToolchainCommands::Pull => tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()?
+            .block_on(pull_toolchain())?,
+
+        // ToolchainCommands::Pull => Ok(()),
         ToolchainCommands::Prune => prune_toolchain(),
         ToolchainCommands::Test => Ok(()),
         ToolchainCommands::Active => {
